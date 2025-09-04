80345ca600d7ac2511e6039d59fdfe34abdda0ab
matedonas <matas@edon.lt>
2025-09-04 12:00:22 +0300

    Move assets to /assets, update Eleventy passthrough, wire templates to new content schema

--- .eleventy.js
+++ .eleventy.js
@@ -1,3 +1,21 @@
+module.exports = function(eleventyConfig) {
+  eleventyConfig.addPassthroughCopy({
+    "assets": "assets"
+  });
+
+  return {
+    dir: {
+      input: ".",
+      includes: "_includes",
+      data: "_data",
+      output: "_site"
+    },
+    markdownTemplateEngine: "njk",
+    htmlTemplateEngine: "njk",
+    dataTemplateEngine: "njk"
+  };
+};
+
 export default function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy({ "assets": "assets" });
 
